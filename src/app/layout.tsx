32fcc6e79f8704f870bd2326ea9eca744756b4f0
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-05-29 12:06:41 +0300

    chore: Add Next.js configuration files and dependencies

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const inter = Poppins({ subsets: ["latin"], weight: ["400", "500", "700", "900"], style: ["normal", "italic"], variable: "--font-poppins"});
+
+export const metadata: Metadata = {
+  title: "Power King Hotel",
+  description: "Discrover the best hotel in Uganda",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <main className="font-normal">
+          {/* Header */}
+          {children}
+          {/* Footer */}
+        </main>
+      </body>
+    </html>
+  );
+}
