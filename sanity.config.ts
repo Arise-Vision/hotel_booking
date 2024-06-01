4afa7777869ce99360a4bdee6c2f4b331cc60357
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-06-01 09:41:54 +0300

    refactor: Update sanity.config.ts to use environment variables for projectId and dataset

--- sanity.config.ts
+++ sanity.config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
   name: 'default',
   title: 'booking',
 
-  projectId: '2xmeg1w3',
-  dataset: 'production',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID as string,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET as string,
   basePath: '/studio',
 
   plugins: [structureTool(), visionTool()],
