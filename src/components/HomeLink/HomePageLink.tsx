e4d0e18e19314a40e76e71fe50919105b055d1d3
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-06-01 09:43:03 +0300

    refactor: Update HomePageLink component with dark mode support

--- src/components/HomeLink/HomePageLink.tsx
+++ src/components/HomeLink/HomePageLink.tsx
@@ -6,7 +6,7 @@ interface HomePageLinkProps {
 
 const HomePageLink = ({classname}: HomePageLinkProps) => {
   return (
-    <Link href='/' className={`bg-gradient-to-br from-transparent via-black/30 to-transparent text-white/30 italic border px-4 py-2 rounded-full font-bold ${classname}`}>Power King</Link>
+    <Link href='/' className={`bg-gradient-to-br from-transparent via-black/30 to-transparent dark:from-white/30 dark:via-transparent dark:to-white/30 text-white/30 italic border px-4 py-2 rounded-full font-bold ${classname}`}>Power King</Link>
   )
 }
 
