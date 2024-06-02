0a4a88e43193b825532ee07fe8e5577997628ecf
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-06-03 02:51:24 +0300

    feat: Add Search component for filtering rooms by type and search query

---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,80 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { ChangeEvent, FC } from 'react';
+
+type Props = {
+  roomTypeFilter: string;
+  searchQuery: string;
+  setRoomTypeFilter: (value: string) => void;
+  setSearchQuery: (value: string) => void;
+};
+
+const Search: FC<Props> = ({
+  roomTypeFilter,
+  searchQuery,
+  setRoomTypeFilter,
+  setSearchQuery,
+}) => {
+  const router = useRouter();
+
+  const handleRoomTypeChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    setRoomTypeFilter(event.target.value);
+  };
+
+  const handleSearchQueryChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleFilterClick = () => {
+    router.push(`/rooms?roomType=${roomTypeFilter}&searchQuery=${searchQuery}`);
+  };
+
+  return (
+    <div className='dark:bg-white/10 bg-black/10 px-4 py-6 rounded-lg mx-12'>
+      <div className='container mx-auto flex gap-4 flex-wrap justify-between items-center'>
+        <div className='w-full md:1/3 lg:w-auto mb-4 md:mb-0'>
+          <label className='block text-sm font-medium mb-2 text-black dark:text-white'>
+            Room Type
+          </label>
+          <div className='relative'>
+            <select
+              value={roomTypeFilter}
+              onChange={handleRoomTypeChange}
+              className='w-full px-4 py-2 capitalize rounded leading-tight dark:bg-black focus:outline-none'
+            >
+              <option value='All'>All</option>
+              <option value='Basic'>Basic</option>
+              <option value='Luxury'>Luxury</option>
+              <option value='Suite'>Suite</option>
+            </select>
+          </div>
+        </div>
+
+        <div className='w-full md:1/3 lg:w-auto mb-4 md:mb-0'>
+          <label className='block text-sm font-medium mb-2 text-black dark:text-white'>
+            Search
+          </label>
+          <input
+            type='search'
+            id='search'
+            placeholder='Search...'
+            className='w-full px-4 py-3 rounded leading-tight dark:bg-black focus:outline-none placeholder:text-black dark:placeholder:text-white border dark:border-white/20'
+            value={searchQuery}
+            onChange={handleSearchQueryChange}
+          />
+        </div>
+
+        <button
+          className='dark:bg-white dark:text-black bg-black text-white py-2 px-3 rounded text-2xl font-bold'
+          type='button'
+          onClick={handleFilterClick}
+        >
+          Search
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
