a453520bdbc2fcc8242a0955262281d65c43602a
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-06-04 00:13:59 +0300

    feat: Add FeaturedRoom component and update Home page

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -2,14 +2,21 @@ import HeroSection from '@/components/HeroSection/HeroSection';
 import PageSearch from '@/components/PageSearch/PageSearch';
 import Gallery from '@/components/Gallery/Gallery';
 import NewsLetter from '@/components/NewsLetter/NewsLetter';
+import FeaturedRoom from '@/components/FeaturedRoom/FeaturedRoom';
+import { getFeaturedRoom } from '@/libs/api'
 
-export default function Home() {
+const Home = async () => {
+  const featuredRoom = await getFeaturedRoom();
+  console.log(getFeaturedRoom);
   return (
     <>
       <HeroSection />
       <PageSearch />
+      <FeaturedRoom featuredRoom={featuredRoom} />
       <Gallery />
       <NewsLetter />
     </>
   );
 }
+
+export default Home;
